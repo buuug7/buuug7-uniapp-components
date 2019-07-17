eb8045ca8c1c4e78159eae9346eed2ed00050733
buuug7 <puguohong@gmail.com>
2019-07-17 17:30:40 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# uni-app components
+
+[uni-app](https://uniapp.dcloud.io/) components
+
+## components
+
++ load-more
++ loading-skeleton
++ simple-datetime-picker
++ tab 
---
+++ lib/load-more/load-more.vue
@@ -0,0 +1,45 @@
+<template>
+  <view class="load-more" @click="_onClick">
+    <text :class="{ more: !more }">{{ more ? "加载更多" : "没有更多了" }}</text>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "load-more",
+  props: {
+    more: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    _onClick() {
+      if (this.more === false) {
+        return;
+      }
+
+      this.$emit("onClick");
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+$active-color: #488ee9;
+
+.load-more {
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  font-size: 16px;
+  color: $active-color;
+}
+
+.more {
+  color: #808080;
+}
+</style>
